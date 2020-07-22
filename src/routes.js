c11efaf6b75794282d0f8d790b264ca384dd6c8f
williamwjd <william.jose.wjd@gmail.com>
2020-07-22 09:56:03 -0300

    service para transformar dados da planilha em json

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { xlsConvertToJson } from "./services/excelToJson";
 
 const routes = Router();
 
 routes.get("/", (req, res) => {
-    return res.json({ ok: true });
+    return res.json(xlsConvertToJson());
 });
 
 export default routes;
---
+++ src/services/excelToJson.js
@@ -0,0 +1,12 @@
+import xlsx from "xlsx";
+import { resolve } from "path";
+
+export const xlsConvertToJson = () => {
+    const plan = xlsx.readFile(
+        resolve(__dirname, "..", "..", "PokemonGo.xlsx"),
+        { cellDates: true }
+    );
+    const planSelected = plan.Sheets["Sheet1"];
+    const data = xlsx.utils.sheet_to_json(planSelected);
+    return data;
+};
