00016e5a71c5661c20331da5e72bde5e1f202c58
Lucca <luccazvps@gmail.com>
2023-05-29 13:01:50 -0300

    feat: add xlsxParser tests and implementation

---
+++ tests/infra/xlsxParser.spec.ts
@@ -0,0 +1,22 @@
+import { XlsxParser } from "../../src/infra/excelParser/xlsxParser";
+import xlsx from "node-xlsx";
+import fs from "fs";
+describe("XlsxParser", () => {
+  const makeSut = () => {
+    return new XlsxParser();
+  };
+  const buffer = fs.readFileSync(`${process.cwd()}/Pokemon Go.xlsx`);
+
+  it("should call xlsx with correct value", async () => {
+    const sut = makeSut();
+    const spy = jest.spyOn(xlsx, "parse");
+    await sut.parser(buffer);
+    expect(spy).toHaveBeenCalledWith(buffer);
+  });
+  it("should return the same values as parse function", async () => {
+    const sut = makeSut();
+    const spy = jest.spyOn(xlsx, "parse").mockImplementationOnce(() => []);
+    const parsedContent = await sut.parser(buffer);
+    expect(parsedContent).toEqual([]);
+  });
+});
